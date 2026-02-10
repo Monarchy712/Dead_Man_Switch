a1a630b0f4c8daa7ce1ed39c711cf6aad516988e
Samyaak Jain <83416299+Monarchy712@users.noreply.github.com>
2026-02-11 00:02:27 +0530

    Add files via upload

---
+++ DeadManSwitch.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract DeadManwitch
+{
+    enum ContractState {
+        Active,
+        Claimable,
+        Claimed
+    }
+
+    address public owner;
+    address public beneficiary;
+    uint public totalAmt;
+    uint public inactivityPeriod;
+    uint public lastCheckIn;
+    ContractState public state;
+
+
+    constructor(address _beneficiary, uint _inactivityPeriod)
+    {
+        owner = msg.sender;
+        lastCheckIn = block.timestamp;
+        beneficiary = _beneficiary;
+        inactivityPeriod = _inactivityPeriod;
+    }
+
+    function deposit() public payable {
+        updateState();
+        totalAmt += msg.value;
+        require(state == ContractState.Active, "Contract not Active!");
+        require(msg.sender == owner, "Only owner can deposit!");
+        lastCheckIn = block.timestamp;
+    }
+
+    function updateState() internal {
+        require(state != ContractState.Claimed, "Already Withdrawn!");
+        if (block.timestamp>lastCheckIn+inactivityPeriod) state = ContractState.Claimable;
+    }
+
+    function checkIn() public{
+        updateState();
+        require(state == ContractState.Active, "Contract not Active!");
+        require(msg.sender == owner, "Only owner can check in!");
+        lastCheckIn = block.timestamp;
+    }
+
+    function claimFunds() public{
+        updateState();
+        require(state == ContractState.Claimable, "Funds not Claimable!");
+        require(msg.sender==beneficiary,"You are not the beneficiary!");
+        payable(beneficiary).call{value:totalAmt}("");
+        state = ContractState.Claimed;
+    }
+
+
+}
